a410343f84abd69127d9c8ba34cd2631098d394f
Lucas Tucker <tucker.lucas.1404@gmail.com>
2023-12-10 13:28:37 +0000

    Finished episode #7.

--- My2DGame/src/main/GamePanel.java
+++ My2DGame/src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import object.SuperObject;
 import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable
@@ -35,7 +36,10 @@ public class GamePanel extends JPanel implements Runnable
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
 	public CollisionChecker cChecker = new CollisionChecker(this);
-	public Player player = new Player(this, keyH);;
+	public AssetSetter aSetter = new AssetSetter(this);
+	public Player player = new Player(this, keyH);
+	public SuperObject obj[] = new SuperObject[10];
+	
 	
 	public GamePanel()
 	{
@@ -46,6 +50,11 @@ public class GamePanel extends JPanel implements Runnable
 		this.setFocusable(true);
 	}
 
+	public void setupGame()
+	{
+		aSetter.setObject();
+	}
+	
 	public void startGameThread()
 	{
 		gameThread = new Thread(this);
@@ -120,7 +129,19 @@ public class GamePanel extends JPanel implements Runnable
 		
 		Graphics2D g2  = (Graphics2D)g;
 		
+		// TILE
 		tileM.draw(g2);
+		
+		// OBJECT
+		for(int i = 0; i < obj.length; i++)
+		{
+			if(obj[i] != null)
+			{
+				obj[i].draw(g2, this);
+			}
+		}
+		
+		// PLAYER
 		player.draw(g2);
 		
 		g2.dispose();
