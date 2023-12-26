807e7e738f6e67c4b0276de121de3bdf645a3108
lucasPCFixo <tucker.lucas.1404@gmail.com>
2023-12-26 15:05:54 +0000

    Finished episode #28.

--- My2DGame/src/entity/Entity.java
+++ My2DGame/src/entity/Entity.java
@@ -45,7 +45,7 @@ public class Entity
 	int hpBarCounter = 0;
 	
 	// CHARACTER STATUS
-	public int type; // 0 = player, 1 = npc, 2 = monster
+
 	public int speed;
 	public String name;
 	public int maxLife;
@@ -65,6 +65,16 @@ public class Entity
 	public int attackValue;
 	public int defenseValue;
 	public String description = "";
+	
+	// TYPE
+	public int type; // 0 = player, 1 = npc, 2 = monster
+	public final int type_player = 0;
+	public final int type_npc = 1;
+	public final int type_monster = 2;
+	public final int type_sword = 3;
+	public final int type_axe = 4;
+	public final int type_shield = 5; 
+	public final int type_consumable = 6; 
 	
 	public Entity(GamePanel gp)
 	{
@@ -100,6 +110,11 @@ public class Entity
 		}
 	}
 	
+	public void use(Entity entity)
+	{
+		
+	}
+	
 	public void update()
 	{
 		setAction();
@@ -111,7 +126,7 @@ public class Entity
 		gp.cChecker.checkEntity(this, gp.monster);
 		boolean contactPlayer = gp.cChecker.checkPlayer(this);
 		
-		if(this.type == 2 && contactPlayer == true)
+		if(this.type == type_monster && contactPlayer == true)	// instance of
 		{
 			if(gp.player.invincible == false)
 			{
