2bb33d42c79eab3c2c8e52afc9114c3e993e05bc
Lucas Tucker <tucker.lucas.1404@gmail.com>
2024-01-01 12:59:35 +0000

    Finised episode #35.

--- My2DGame/src/main/GamePanel.java
+++ My2DGame/src/main/GamePanel.java
@@ -7,11 +7,13 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import entity.Entity;
@@ -40,6 +42,7 @@ public class GamePanel extends JPanel implements Runnable
 	int screenHeight2 = screenHeight;
 	BufferedImage tempScreen;
 	Graphics2D g2;
+	public boolean fullScreenOn = false;
 	
 	// FPS
 	int FPS = 60;
@@ -72,6 +75,7 @@ public class GamePanel extends JPanel implements Runnable
 	public final int pauseState = 2;
 	public final int dialogueState = 3;
 	public final int characterState = 4;
+	public final int optionsState = 5;
 	
 	public GamePanel()
 	{
@@ -94,7 +98,7 @@ public class GamePanel extends JPanel implements Runnable
 		tempScreen = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
 		g2 = (Graphics2D)tempScreen.getGraphics();
 		
-		// setFullScreen();
+		//setFullScreen();
 	}
 	
 	public void setFullScreen()
