f0a2fdb3f0bbbc50825aeaf665f5d13ce9489145
lucasPCFixo <tucker.lucas.1404@gmail.com>
2024-01-02 17:49:36 +0000

    Finished episode #36.

--- My2DGame/src/main/GamePanel.java
+++ My2DGame/src/main/GamePanel.java
@@ -56,6 +56,7 @@ public class GamePanel extends JPanel implements Runnable
 	public AssetSetter aSetter = new AssetSetter(this);
 	public UI ui = new UI(this);
 	public EventHandler eHandler = new EventHandler(this);
+	Config config = new Config(this);
 	Thread gameThread;
 	
 	// ENTITY AND OBJECT
@@ -98,7 +99,10 @@ public class GamePanel extends JPanel implements Runnable
 		tempScreen = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
 		g2 = (Graphics2D)tempScreen.getGraphics();
 		
-		//setFullScreen();
+		if(fullScreenOn == true)
+		{
+			setFullScreen();
+		}
 	}
 	
 	public void setFullScreen()
