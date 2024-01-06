71a4e26b2d8b46b474b2a199060760b20b2d54b6
Lucas Tucker <tucker.lucas.1404@gmail.com>
2024-01-06 19:00:12 +0000

    Finished episode #44.

--- My2DGame/src/main/GamePanel.java
+++ My2DGame/src/main/GamePanel.java
@@ -19,6 +19,7 @@ import javax.swing.JPanel;
 import ai.PathFinder;
 import entity.Entity;
 import entity.Player;
+import environment.EnvironmentManager;
 import tile.TileManager;
 import tile_interactive.InteractiveTile;
 
@@ -61,6 +62,7 @@ public class GamePanel extends JPanel implements Runnable
 	public EventHandler eHandler = new EventHandler(this);
 	Config config = new Config(this);
 	public PathFinder pFinder = new PathFinder(this);
+	EnvironmentManager eManager = new EnvironmentManager(this);
 	Thread gameThread;
 	
 	// ENTITY AND OBJECT
@@ -102,7 +104,8 @@ public class GamePanel extends JPanel implements Runnable
 		aSetter.setNPC();
 		aSetter.setMonster();
 		aSetter.setInteractiveTile();
-		//playMusic(0);
+		eManager.setup();
+		
 		gameState = titleState;
 		
 		tempScreen = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
@@ -381,6 +384,9 @@ public class GamePanel extends JPanel implements Runnable
 			
 			// EMPTY ENTITY LIST
 			entityList.clear();
+			
+			// ENVIRONMENT
+			eManager.draw(g2);
 			
 			// UI
 			ui.draw(g2);
