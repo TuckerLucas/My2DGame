926459177395f7aaa0b1e25f4bf4c26bd7ced6c3
Lucas Tucker <tucker.lucas.1404@gmail.com>
2024-01-14 10:51:28 +0000

    Finished episode #47.

--- My2DGame/src/main/KeyHandler.java
+++ My2DGame/src/main/KeyHandler.java
@@ -68,6 +68,11 @@ public class KeyHandler implements KeyListener
 		{
 			tradeState(code);
 		}
+		// MAP STATE
+		else if(gp.gameState == gp.mapState)
+		{
+			mapState(code);
+		}
 	}
 
 	public void titleState(int code)
@@ -150,6 +155,21 @@ public class KeyHandler implements KeyListener
 		if(code == KeyEvent.VK_ESCAPE)
 		{
 			gp.gameState = gp.optionsState;
+		}
+		if(code == KeyEvent.VK_M)
+		{
+			gp.gameState = gp.mapState;
+		}
+		if(code == KeyEvent.VK_X)
+		{
+			if(gp.map.miniMapOn == false)
+			{
+				gp.map.miniMapOn = true;
+			}
+			else
+			{
+				gp.map.miniMapOn = false;
+			}
 		}
 		
 		// DEBUG 
@@ -374,6 +394,13 @@ public class KeyHandler implements KeyListener
 		}
 	}
 	
+	public void mapState(int code)
+	{
+		if(code == KeyEvent.VK_M)
+		{
+			gp.gameState = gp.playState;
+		}
+	}
 	
 	public void playerInventory(int code)
 	{
